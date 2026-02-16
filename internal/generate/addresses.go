28f13b70ea833fbf250aafda1c102d591553be75
Arkadiy Kukarkin <parkan@gmail.com>
2026-02-16 16:34:42 +0100

    pr comments

--- internal/generate/addresses.go
+++ internal/generate/addresses.go
@@ -149,7 +149,7 @@ var (
 `))
 
 func main() {
-	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 60*time.Second)
 	defer cancel()
 
 	fmt.Println("reading mainnet addresses...")
